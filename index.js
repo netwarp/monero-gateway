4f74ba60fbd3575f7890a8aa1b1f495c4f202e5e
netwarp <resethread@gmail.com>
2021-01-24 12:50:28 +0100

    up app

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const  nunjucks = require('nunjucks')
 
+const redis = require('redis')
+const redis_client = redis.createClient()
+redis_client.on("error", function(error) {
+	console.error(error)
+})
+
+const monerojs = require("monero-javascript")
+
 const PORT = 8080
 
 nunjucks.configure('views', {
@@ -12,10 +20,23 @@ nunjucks.configure('views', {
 	watch: true,
 })
 
+const subscriber = redis.createClient()
+const publisher = redis.createClient()
+
+subscriber.on("message",(channel, message) => {
+    
+	io.sockets.emit('tx_done', message)
+})
+
+subscriber.subscribe("tx");
+
+
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-	res.render('template.html', {
+
+app.get('/', async (req, res) => {
+
+	await res.render('gateway.html', {
 		name: "world" // renders ./pages/Home.html with props {name: "world"}
 	});
 })
---
+++ resources/js/App.svelte
@@ -0,0 +1,47 @@
+<script>
+	import ioClient from 'socket.io-client'
+	import swal from 'sweetalert'
+
+
+	let io = ioClient()
+
+
+	io.on('tx_done', (tx) => {
+		swal('Good job!', 'You clicked the button!', 'success')
+	})
+</script>
+
+<div class="dero-payment-container">
+	<div class="dero-payment-header">
+		<div class="title">
+			Dero Payment Button
+		</div>
+	</div>
+	<div class="dero-payment-body">
+		
+		<div class="alert success hide">
+			
+		</div>
+
+		<canvas id="canvas"></canvas>
+		
+		<div class="amount">
+			
+		</div>
+
+		<input class="address" id="address" value="">
+		
+		<div class="timer">
+			
+		</div>
+
+		<div class="timer-container">
+			<div class="timer-bar">
+				
+			</div>
+		</div>
+	</div>
+	<div class="dero-payment-footer">
+		Privacy Together
+	</div>
+</div>
