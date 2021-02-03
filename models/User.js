23b188c8aee6fe5eb68e78b1dddb2e10eaa6e3b9
netwarp <resethread@gmail.com>
2021-02-03 17:12:41 +0100

    add timestamp to user model

--- models/User.js
+++ models/User.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const schema = new Schema({
-    name:  String, // String is shorthand for {type: String}
-    password: String
- 
-})
+const schema = new Schema(
+	{
+    	name:  String, // String is shorthand for {type: String}
+    	password: String
+	},
+	{
+		timestamps: 
+		{ 
+			createdAt: 'created_at', 
+			updatedAt: 'updated_at' 
+		}
+	}
+)
 
 const User = mongoose.model('User', schema)
 
