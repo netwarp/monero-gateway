3aefc6f56caacbae2c01dcb6cfcf3af0fea7cb0d
netwarp <resethread@gmail.com>
2021-02-09 16:24:51 +0100

    create processing page

---
+++ resources/js/dashboard/components/Processing.svelte
@@ -0,0 +1,28 @@
+<script>
+    import ioClient from 'socket.io-client'
+
+    let io = ioClient('/dashboard', {
+        query: {
+            page: 'dashboard'
+        }
+    })
+
+    let payments = []
+
+    io.emit('refresh-processing-client-ask-server')
+
+    io.on('refresh-processing-server-answer-client', (data) => {
+        console.log(data)
+    })
+</script>
+
+<div class="row mt-3">
+    <h1 class="h3">Processing</h1>
+    <div class="col-md">
+        <div class="card mt-3">
+            <div class="card-header">
+
+            </div>
+        </div>
+    </div>
+</div>
